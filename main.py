038bf0e5b5794b5d875d0fa19e00714c2850ad35
Дмитрий <flowhack@outlook.com>
2020-12-31 14:39:03 +0400

    edit time send -> 1min and fix bug

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 logging.basicConfig(
-    filename='main.log',
-    format='!levelname!: %(asctime)s %(message)s'
+    level=logging.INFO,
+    filename='main.log', filemode='a',
+    format='-'*50 + '\n!%(levelname)s!: %(asctime)s: %(message)s\n' + '-'*50
 )
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
@@ -22,26 +23,41 @@ HEADERS_FOR_YANDEX = {
 
 
 def parse_homework_status(homework):
-    homework_name = homework['homework_name']
-    if homework['status'] == 'reviewing':
+    homework_name = homework.get('homework_name')
+    status = homework.get('status')
+    if status == 'reviewing':
         return f'У вас начали проверять работу "{homework_name}"!'
-    if homework['status'] == 'rejected':
+    elif status == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
-    else:
+    elif status == 'approved':
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+    else:
+        return 'Пришёл неверный статус ответа от Яндекс Практикума'
+
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
 def get_homework_statuses(current_timestamp):
+    current_timestamp = int(time.time()) if current_timestamp is None else current_timestamp
     form_date_for_homework_get = {
         'from_date': current_timestamp
     }
-    homework_statuses = requests.get(
-        URL_GET_HOMEWORK,
-        headers=HEADERS_FOR_YANDEX,
-        params=form_date_for_homework_get
-    )
-    return homework_statuses.json()
+
+    try:
+        homework_statuses = requests.get(
+            URL_GET_HOMEWORK,
+            headers=HEADERS_FOR_YANDEX,
+            params=form_date_for_homework_get
+        )
+
+        return homework_statuses.json()
+
+    except requests.exceptions.ConnectionError as e:
+        logging.error(f'Не удаётся установить соединение: {e}')
+    except ValueError as e:
+        logging.error('Возвращается битый json  в функции получания статуса дз')
+
+    return {}
 
 
 def send_message(message, bot_client):
@@ -55,12 +71,14 @@ def main():
             bot_client_telegram = telegram.Bot(token=TELEGRAM_TOKEN)
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
+                message = parse_homework_status(new_homework.get('homeworks')[0])
                 send_message(
-                    parse_homework_status(new_homework.get('homeworks')[0]),
+                    message,
                     bot_client_telegram
                 )
+                logging.info(f'Удачно отправил сообщение\n\n{message}')
             current_timestamp = new_homework.get('current_date', current_timestamp)
-            time.sleep(300)
+            time.sleep(60)
 
         except Exception as e:
             logging.error(f'Бот столкнулся с ошибкой: {e}')
